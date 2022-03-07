665ca4b721c11dac68820f5841139d62b10a01e6
Marcolux <mccolucci1@gmail.com>
2022-03-06 22:06:08 -0600

    cleaning

--- src/components/Category.js
+++ src/components/Category.js
@@ -9,10 +9,11 @@ import env from 'react-dotenv';
 const Category= (props)=>{
 
     let history = useNavigate()
-    
-
+    // we use setRecipeId context to temporary store the id so SinglePageFromBackend can use it 
     const { recipeIdState } = useContext(Context);
     const [recipeId,setRecipeId] = recipeIdState
+
+    // we set CategId and categoryName context to use the filter function in all recipes page
 
     const { categIdState } = useContext(Context);
     const [categId,setCategId] = categIdState
@@ -22,52 +23,50 @@ const Category= (props)=>{
 
     const [recipesInCategory, setRecipesInCategory] = useState([])
 
+    
+    // props we need for the backend call 
     let catIdBackend= props.category.id
-
     let userId = localStorage.getItem('userId')
-
+    // backend call function to get all the recipes in a specific category
     const getRecipeCategory=()=>{
     axios.get(`${env.BACKEND_URL}/category/${userId}/${catIdBackend}/recipes`)
     .then((response)=>{
         // console.log(response.data)
+        // we store them in a recipesInCategory state
         setRecipesInCategory(response.data)
     })}
+
     useEffect(getRecipeCategory,[])
     
 
+    // function to delete the recipes in a category 
     const deleteRecipe= (i)=>{
-
         recipesInCategory.splice(i,1)
-        let array1 = recipesInCategory
-        setRecipesInCategory(array1)
+        // and update the list
+        setRecipesInCategory([...recipesInCategory])
     }
     
-    const deleteCategory= (i)=>{
-
-    props.categoriesUser.splice(i,1)
-    let array = props.categoriesUser
-    props.setCategoriesUser(array)
-    }
 
     return(
         <>
         <p className='categoryTitle'>{props.category.name}</p>
         <div className="singleCat">
-                
+                {/* we map through the list of the recipes in our category */}
             {recipesInCategory.map((recipe,i)=>{
 
                 return(
+                    // and we show every single recipe with a remove button that removes it from the category 
                     <div className="singleResult" key={i}>
-                        <button className='removeRecipe' onClick={
-                            ()=>{
-                            
+                        <button className='removeRecipe' onClick={()=>{
                             axios.delete(`${env.BACKEND_URL}/category/${catIdBackend}/${recipe.id}`)
                             deleteRecipe(i)
-                            setRecipesInCategory([...recipesInCategory])
                         }}>x</button>
                         <div className="resultPic" style={{backgroundImage:recipe.picture}}></div>
+                        {/* if we click to the recipe title we'll open the recipe page in saved-recipe  */}
                         <p className='linksRecipe' onClick={()=>{
+                            // we use setRecipeId context to temporary store the id so SinglePageFromBackend can use it 
                             setRecipeId(recipe.id)
+                            // we need to navigate to /saved-recipe route in order to open the singleRecipePage component
                             history('/saved-recipe')
                             }}>{recipe.name}</p> 
                     
@@ -76,6 +75,7 @@ const Category= (props)=>{
             })}
         </div>
         <button className='addRecipeButton' onClick={()=>{
+            // we set CategId and categoryName context to use the filter function in all recipes page
             setCategId(props.category.id)
             history('/all-the-recipes')
             setCategoryName(props.category.name)
--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -6,23 +6,27 @@ import env from 'react-dotenv';
 
 import NavigationBar from "../components/NavigationBar"
 import AddCategory from '../components/AddCategory';
-import CategoriesInPage from '../components/CategoriesInPage';
+// import CategoriesInPage from '../components/CategoriesInPage';
 import Category from '../components/Category';
 
 const UserPage = ()=>{
 
     const { userState } = useContext(Context);
     const [user, setUser] = userState
+    // useState to create a category
     const [createCategory, setCreateCategory] = useState(false)
     const [categoriesUser, setCategoriesUser] =useState([])
-    console.log(user)
+    // console.log(user)
     // useEffect(setCreateCategory,[])
 
+    // backend call to get the user's categories 
     const getCategories=()=>{
         const userId=localStorage.getItem('userId')
     
         axios.get(`${env.BACKEND_URL}/category/all/${userId}`)
-        .then((response)=>{setCategoriesUser(response.data)})
+        .then((response)=>{
+            // after we have the response we set a state to store them
+            setCategoriesUser(response.data)})
     }
     
     useEffect(getCategories,[createCategory])
@@ -37,6 +41,7 @@ const UserPage = ()=>{
                         :
                         <div>
                             <p>Add a New Category</p>
+                            {/* once we push add we switch state to createCategory and an addCategory component is going to show */}
                             <button className='addFromBackend' onClick={()=>{
                                 setCreateCategory(true)}
                             } >ok</button>
@@ -47,16 +52,18 @@ const UserPage = ()=>{
                 {categoriesUser?
                      <div className='allCategories'>
                         <div className='CategorySection' >
+                            {/* ???? used to check if the categoriesUser is loaded  */}
                             {categoriesUser?.map((category,i)=>{
                                 return(
                                     <div className="allTheCategories" key={i}>
                                         <button className='deleteCat' onClick={()=>{
+                                            // axios call to backend to delete category and the relations with all the recipes
                                             axios.delete(`${env.BACKEND_URL}/category/${category.id}`)
+                                            // then we update the page
                                             categoriesUser.splice(i,1)
-                                            let array = categoriesUser
-                                            setCategoriesUser(array)
-                                        
-                                        setCategoriesUser([...categoriesUser])
+                                            // let array = categoriesUser
+                                            // setCategoriesUser(array)
+                                            setCategoriesUser([...categoriesUser])
                                             }
                                         }>Remove</button>
                                         <Category key={i} className='categorySingle'  category={category} categoriesUser={categoriesUser} setCategoriesUser={setCategoriesUser} />
