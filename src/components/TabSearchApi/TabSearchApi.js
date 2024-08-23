58afe2bf18c8d3fb27de2c4a6b3d094a1d476c1a
Marcolux <mccolucci1@gmail.com>
2024-08-23 18:23:47 -0500

    Search API form style completed

--- src/components/TabSearchApi/TabSearchApi.js
+++ src/components/TabSearchApi/TabSearchApi.js
@@ -4,9 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const react_1 = __importDefault(require("react"));
+require("./style/tab-search-api.css");
 const TabSearchAPI = ({ tabSwitch }) => {
-    console.log('tabSwitch:', tabSwitch); // Check the value of tabSwitch
-    return (react_1.default.createElement("div", { className: tabSwitch === 'searchApi' ? 'pageContent searchApi active' : 'pageContent searchApi' },
-        react_1.default.createElement("div", null, "Search from API")));
+    const classList = tabSwitch === 'searchApi' ? 'pageContent searchApi active' : 'pageContent searchApi';
+    const searchForm = () => {
+        return (react_1.default.createElement("form", { className: "formApi", onSubmit: () => { } },
+            react_1.default.createElement("p", { className: "searchLabel" }, "Search By"),
+            react_1.default.createElement("select", { className: "searchInput mg-l-Xl", onChange: (e) => { } },
+                react_1.default.createElement("option", null, "select the search criteria"),
+                react_1.default.createElement("option", { value: "byName" }, "Recipe Name"),
+                react_1.default.createElement("option", { value: "byIngredients" }, "Recipe Ingredients")),
+            react_1.default.createElement("input", { className: "searchInput mg-x-Xl", value: "", onChange: (e) => { } }),
+            react_1.default.createElement("button", { className: "button dark small" }, "Search")));
+    };
+    return (react_1.default.createElement("div", { className: classList },
+        react_1.default.createElement("div", { className: "searchFromApiCont" },
+            react_1.default.createElement("p", { className: "apiSearchHeader" }, "Search Recipes with Spoonacular API"),
+            searchForm())));
 };
 exports.default = TabSearchAPI;
--- src/components/TabSearchApi/TabSearchApi.tsx
+++ src/components/TabSearchApi/TabSearchApi.tsx
@@ -1,11 +1,38 @@
 import React from "react";
+import './style/tab-search-api.css'
 
 
 const TabSearchAPI = ({tabSwitch}: {tabSwitch:string} ): React.JSX.Element => {
-    console.log('tabSwitch:', tabSwitch); // Check the value of tabSwitch
+    const classList = tabSwitch === 'searchApi' ? 'pageContent searchApi active' : 'pageContent searchApi'
+
+    const searchForm = () => {
+        return (
+            <form className="formApi" onSubmit={() => {}}>
+                <p className="searchLabel">Search By</p>
+                <select
+                    className="searchInput mg-l-Xl"
+                    onChange={(e) => { }}
+                >
+                    <option>select the search criteria</option>
+                    <option value={"byName"}>Recipe Name</option>
+                    <option value={"byIngredients"}>Recipe Ingredients</option>
+                </select>
+
+                <input
+                    className="searchInput mg-x-Xl"
+                    value={""}
+                    onChange={(e) => {}}
+                />
+                <button className="button dark small">Search</button>
+            </form>
+        )
+    }
     return (
-        <div className={tabSwitch === 'searchApi' ? 'pageContent searchApi active' : 'pageContent searchApi'}>  
-            <div >Search from API</div>
+        <div className={classList}>  
+            <div className="searchFromApiCont">
+                <p className="apiSearchHeader">Search Recipes with Spoonacular API</p>
+                {searchForm()}
+            </div>
         </div>
     )
 
